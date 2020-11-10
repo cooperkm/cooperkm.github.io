e7389cc4f3922b3d8053e8a30cb1f51e6fc3bf5a
kylecooper <kyle.cooper@morgan6.com>
2020-11-10 18:45:14 -0500

    fixed safari

--- js/demo.js
+++ js/demo.js
@@ -586,6 +586,8 @@ var multicodec = function() {
         source.dash = '//bitmovin-a.akamaihd.net/webpages/demos/content/multi-codec/hevc/stream.mpd';
     } else if (browser === BROWSER.SAFARI) {
         selectedCodec = CODEC.H265;
+        source.hls = '//bitmovin-a.akamaihd.net/webpages/demos/content/multi-codec/hevc/stream_fmp4.m3u8';
+        document.getElementById('stats-panels-wrapper').style.display = 'none'; //hiding the performance box as we do not get this data from safari
     }
 
     var selectedCodecElements = document.getElementsByClassName('selected-codec');
